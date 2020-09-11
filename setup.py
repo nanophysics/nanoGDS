4b763ad7d945da4e559e82ac2cdb4ce03aa02568
Max Ruckriegel <max.ruckriegel@gmx.de>
2020-09-11 09:44:45 +0200

    Add requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+import setuptools
+
+
+requirements = ["numpy>=1.13", "setuptools>=40.1.0", "gdspy"]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mygds",
+    description="Zurich Instruments tools for high level device control",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/zhinst/zhinst-toolkit",
+    author="Max Ruckriegel",
+    author_email="maxr@ethz.ch",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords="gds gdspy",
+    packages=setuptools.find_namespace_packages(exclude=[], include=["mygds.*"]),
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires=">=3.6",
+    zip_safe=False,
+)
